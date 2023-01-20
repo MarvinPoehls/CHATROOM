a71ec8b259e7717f2e10e9d2136babd2d1e0d322
MarvinPoehls <112632120+MarvinPoehls@users.noreply.github.com>
2023-01-20 15:08:25 +0100

    Delete Chatroom directory

--- Chatroom/Views/error.php
+++
@@ -1,8 +0,0 @@
-<div class="card bg-danger text-dark">
-    <div class="card-header">
-        ERROR
-    </div>
-    <div class="card-body">
-        <p class="card-text">Message: <?php echo $controller->getException()->getMessage(); ?></p>
-    </div>
-</div>
--- Chatroom/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $data = [
-    "path" => "http://localhost/azubiManagement/",
-    "salt" => "0ot0ewß093l2d:;:_-#1speßKMÖwüv",
-    "servername" => "localhost",
-    "username" => "root",
-    "password" => "",
-    "dbname" => "test"
-];
