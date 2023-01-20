08c42291c7bc3729a69e77636b55bd5b7f02bdf9
MarvinPoehls <112632120+MarvinPoehls@users.noreply.github.com>
2023-01-20 15:07:15 +0100

    Add files via upload

---
+++ Chatroom/config.php
@@ -0,0 +1,9 @@
+<?php
+    $data = [
+    "path" => "http://localhost/azubiManagement/",
+    "salt" => "0ot0ewß093l2d:;:_-#1speßKMÖwüv",
+    "servername" => "localhost",
+    "username" => "root",
+    "password" => "",
+    "dbname" => "test"
+];
